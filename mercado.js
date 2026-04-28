9865abcda616f4d9580bd909e0dcb6be52b6adc6
khayck <khayck.fronza@escola.pr.gov.br>
2026-04-28 12:11:54 -0300

    atualizacao

--- mercado.js
+++ mercado.js
@@ -4,14 +4,31 @@ const porta = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.post ('/finalizar-venda', (req,res) => {
-    let procureProduto = req.body.procureProduto
+app.post('/finalizar-venda', (req, res) => {
+    let nomeProduto = req.body.nomeProduto
     let precoUnitario = req.body.precoUnitario
     let quantidade = req.body.quantidade
     let cupomDesconto = req.body.cupomDesconto
 
-    if (quantidade <= 0){
-        return res.status(400)
+    valorTotal = quantidade * precoUnitario
+
+    if (cupomDesconto === "PROMO20") {
+        valorTotal = valorTotal - 20 / 100;
+    }
+    if (quantidade <= 0) {
+        return res.status(400).json({
+            sucesso: false,
+            erro: "Quantidade inválida!"
+        })
 
     }
+ return res.status(201).json({
+        sucesso: true,
+        mensagem: `Compra realizada com sucesso! ${valorTotal + nomeProduto}`,
+    })
+
 })
+
+app.listen(porta, () => {
+    console.log(`Servidor rodando! Aguardando o formulário HTML na porta ${porta}...`)
+});
